13c1fcbe6fea39c0897514aa431b8a39df4cbdd9
javierquinterocl <jaquinterocl@ufpso.edu.co>
2023-09-13 17:44:32 -0500

    Terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "haken" : [ '"Hanken Grotesk"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
